da57ee82f25d8b4de1db2b7811e36103e337c3ee
hyunjunghun <dnel8332@naver.com>
2025-11-25 16:55:58 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -21,13 +21,34 @@ public class Main {
 		
 		//DFS, BFS
 		System.out.println("1. 그래프 탐방 수행 결과");
-		
+		for (int j=0; j<gl.graphs1.size(); j++) {
+			Graph g = gl.graphs1.get(j);
+			DFS d = new DFS(g.graph);
+			BFS b = new BFS(g.graph);
+			System.out.println("그래프 ["+(j+1)+"]");
+			System.out.println("----------------------------");
+			System.out.println("깊이 우선 탐색");
+			d.DFSOutput();
+			System.out.println("너비 우선 탐색");
+			b.BFSOutput();
+			System.out.println("============================");			
+		}
 		
 		//Dijkstra
 		System.out.println("2. 최단 경로 구하기 수행 결과");
+		for (int j=0; j<gl.graphs2.size(); j++) {
+			Graph g = gl.graphs2.get(j);
+			Dijkstra d = new Dijkstra(g.graph);
+			System.out.println("그래프 ["+(j+1)+"]");
+			System.out.println("----------------------------");
+			System.out.println("시작점 : 1");
+			d.DijkstraOutput();
+			System.out.println("============================");			
+		}
 		
 		
 	}
 
 }
+
 
