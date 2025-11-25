c14b02d57cae57bb0d8ec5c3416f3888eb8985e0
hyunjunghun <dnel8332@naver.com>
2025-11-25 16:46:41 +0900

    Remove commented code and clean Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 package DM_project1;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-//import java.util.Arrays;
 
 public class Main {
 
@@ -19,23 +18,6 @@ public class Main {
 			System.out.println("돌아가");
 		}
 		
-//		//input1 그래프 출력
-//		for (int j=0; j<gl.graphs1.size(); j++) {
-//			Graph g = gl.graphs1.get(j);
-//			System.out.println("graphs1:"+j);
-//			for (int i = 0; i < g.graph.length; i++) {
-//			    System.out.println(Arrays.toString(g.graph[i]));
-//			}
-//		}
-//		
-//		//input2 그래프 출력
-//		for (int j=0; j<gl.graphs2.size(); j++) {
-//			Graph g = gl.graphs2.get(j);
-//			System.out.println("graphs2:"+j);
-//			for (int i = 0; i < g.graph.length; i++) {
-//			    System.out.println(Arrays.toString(g.graph[i]));
-//			}
-//		}
 		
 		//DFS, BFS
 		System.out.println("1. 그래프 탐방 수행 결과");
@@ -48,3 +30,4 @@ public class Main {
 	}
 
 }
+
