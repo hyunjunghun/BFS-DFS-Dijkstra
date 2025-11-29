1444260cb61270a1db85432e2a6c928ac9b746f9
skybloom10 <sumyintmyataung10@gmail.com>
2025-11-29 12:05:35 +0900

    Update DFS.java

--- DFS.java
+++ DFS.java
@@ -1,14 +1,38 @@
 package DM_project1;
 
 public class DFS {
-	int[][] graph;
-	
-	public DFS(int[][] graph) {
-		this.graph = graph;
-	}
-	
-	public void DFSOutput() {
-		
-	}
+    int[][] graph;
+    boolean[] visited;
+    int n;
+
+    public DFS(int[][] graph) {
+        this.graph = graph;
+        this.n = graph.length;
+        this.visited = new boolean[n];
+    }
+
+    public void DFSOutput() {
+        dfs(0);
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+
+        System.out.println();
+    }
+
+    private void dfs(int v) {
+        visited[v] = true;
+        System.out.print((v + 1) + " ");
+
+        for (int i = 0; i < n; i++) {
+            if (graph[v][i] != 0 && !visited[i]) {
+                dfs(i);
+            }
+        }
+    }
 }
+
 
