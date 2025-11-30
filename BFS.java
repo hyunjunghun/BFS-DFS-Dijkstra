4daaefe620ba27af4808657184613f7c7decfbae
him <powerful4582@gmail.com>
2025-11-30 17:10:02 +0900

    BFS.java

--- BFS.java
+++ BFS.java
@@ -16,9 +16,9 @@ public class BFS {
 	}
 	
 	public void BFSOutput() {
-        this.visited = new boolean[n]; // 그래프 2개 이상일떄 초기화해줘야됨
+        this.visited = new boolean[n]; // 그래프 2개 이상일떄 초기화
         bfs(1);
-        System.out.println(""); //줄바꿈은 여기서
+        System.out.println("");
 
 	}
 
@@ -27,24 +27,24 @@ public class BFS {
         s = s-1;
         int index = 0;
         Queue<Integer> array = new LinkedList<>();
-        visited[s] = true; //방문한거 받아줌
+        visited[s] = true;
         array.offer(s); //{0}
         boolean First = true;
 
         while (!array.isEmpty()) {
-            int peak = array.poll(); //0 //1
+            int peak = array.poll();
 
             if(First) {
                 System.out.print(peak+1);
                 First = false;
             } else
-                System.out.print(" - " + (peak+1)); //처음에는 그냥 숫자만 출력 그 뒤는 전부 하이픈까지 같이 출력
+                System.out.print(" - " + (peak+1)); 
 
 
             for(int i = 0 ; i < n ; i++) {
                 if (graph[peak][i] == 1 && visited[i] != true) {
-                    array.add(i); //{1,2}
-                    visited[i] = true; //{true , true , true}
+                    array.add(i);
+                    visited[i] = true;
                 }
             }
         }
@@ -52,3 +52,4 @@ public class BFS {
 
     }
 }
+
